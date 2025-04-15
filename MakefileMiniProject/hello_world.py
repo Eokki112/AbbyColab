ff417be69258856b54b44592c233c11d60a22913
Your Name <you@example.com>
2025-04-15 15:06:50 -0400

    Merged with remote main branch

---
+++ MakefileMiniProject/hello_world.py
@@ -0,0 +1,8 @@
+# hello_world.py
+with open("output1.txt", "w") as f:
+    f.write("This is the output of script 1.\n")
+    f.write("Line 2\n")
+    f.write("Line 3\n")
+    f.write("Line 4\n")
+    f.write("Line 5\n")
+
---
+++ MakefileMiniProject/script2.py
@@ -0,0 +1,8 @@
+# script2.py
+with open("output2.txt", "w") as f:
+    f.write("This is script 2.\n")
+    f.write("We love JC./n")
+    f.write("I need another break though.\n")
+    f.write("Almost done with this semester\n")
+    f.write("Finally!\n")
+
